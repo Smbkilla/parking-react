b368b6e37ece81907ff925f973196fb460c49b03
Matija Katanec <mkatanec@croz.net>
2020-04-30 22:19:38 +0200

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Route} from "react-router-dom";
 import Entrances from "./containers/Entrances";
 import Floors from "./containers/Floors";
 import Home from "./containers/Home";
+import NavBar from "./components/NavBar";
 import ParkingSpaces from "./containers/ParkingSpaces";
 
 import './App.css';
@@ -12,10 +13,11 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/floors' component={Floors}/>
-        <Route exact path='/parkingSpaces' component={ParkingSpaces}/>
-        <Route exact path='/entrances' component={Entrances}/>
+            <NavBar/>
+            <Route exact path='/' component={Home}/>
+            <Route path='/floors' component={Floors}/>
+            <Route path='/parkingSpaces' component={ParkingSpaces}/>
+            <Route path='/entrances' component={Entrances}/>
       </BrowserRouter>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, {useState} from 'react';
+import {useHistory} from "react-router-dom";
 
-export default function NavBar(props){
-  return(
-    <div/>
+import AppBar from "@material-ui/core/AppBar";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from '@material-ui/icons/Menu';
+import LocalParkingIcon from '@material-ui/icons/LocalParking';
+import HomeIcon from '@material-ui/icons/Home';
+import InputIcon from '@material-ui/icons/Input';
+import ViewStreamIcon from '@material-ui/icons/ViewStream';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+
+export default function NavBar(props) {
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const history = useHistory();
+
+  const onMenuClicked = () => {
+    setOpenDrawer(!openDrawer);
+  };
+
+  const onNavigationButtonClick = (path) => () => {
+    history.push(path);
+    setOpenDrawer(false);
+  };
+
+  return (
+    <div className="root">
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <IconButton edge="start" className="toolbar-icon" color="inherit" aria-label="menu" onClick={onMenuClicked}>
+            <MenuIcon/>
+          </IconButton>
+          <Typography className="toolbar-title">
+            ParkMe
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={openDrawer}
+              anchor="left">
+        <div className="drawer-header">
+          <IconButton onClick={onMenuClicked}>
+            <ChevronLeftIcon/>
+          </IconButton>
+        </div>
+        <Divider/>
+        <List>
+          <ListItem button key="Home" onClick={onNavigationButtonClick("/")}>
+            <ListItemIcon><HomeIcon/></ListItemIcon>
+            <ListItemText primary={"Home"}/>
+          </ListItem>
+          <ListItem button key="Parking space" onClick={onNavigationButtonClick("/parkingSpaces")}>
+            <ListItemIcon><LocalParkingIcon/></ListItemIcon>
+            <ListItemText primary={"Parking space"}/>
+          </ListItem>
+          <ListItem button key="Entrance" onClick={onNavigationButtonClick("/entrances")}>
+            <ListItemIcon><InputIcon/></ListItemIcon>
+            <ListItemText primary={"Entrance"}/>
+          </ListItem>
+          <ListItem button key="Floor" onClick={onNavigationButtonClick("/floors")}>
+            <ListItemIcon><ViewStreamIcon/></ListItemIcon>
+            <ListItemText primary={"Floor"}/>
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
   )
 }
